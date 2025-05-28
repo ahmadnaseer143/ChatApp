b2618960a43fb6dd6a2eba5f2259b5b7d53dc735
ahmadnaseer143 <anaseer942@gmail.com>
2025-05-27 19:59:12 -0500

    routes and controllers added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signupUser = (req, res) => {
+  res.send("Signup ROUTE");
+};
+
+export const loginUser = (req, res) => {
+  res.send("LOGIN ROUTE");
+};
+
+export const logoutUser = (req, res) => {
+  res.send("Logout ROUTE");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/auth.routes.js";
 const app = express();
 dotenv.config();
 
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Herllo World");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
