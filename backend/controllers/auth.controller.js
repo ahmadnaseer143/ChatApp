8eac8ede061fc0d6cbef79160f8f5c90df146f5c
ahmadnaseer143 <anaseer942@gmail.com>
2025-05-28 16:15:02 -0500

    Password hashed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signupUser = async (req, res) => {
   try {
@@ -9,30 +10,37 @@ export const signupUser = async (req, res) => {
     const user = await User.findOne({ username });
     if (user) return res.status(400).json({ error: "Username already exists" });
 
+    // has passsword using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const boyProfilePic = "https://avatar.iran.liara.run/public/boy";
     const girlProfilePic = "https://avatar.iran.liara.run/public/girl";
 
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (error) {
     console.log("Error in Signing up controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
-  res.send("Signup ROUTE");
 };
 
 export const loginUser = (req, res) => {
