a6b7a6d6390866995249dbeedbfd750b9b639fec
ahmadnaseer143 <anaseer942@gmail.com>
2025-05-29 15:17:27 -0500

    send message complete

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,35 @@
-export const sendMessage = (req, res) => {
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
   try {
-    console.log(req.params.id);
-    console.log("message setInterval");
-  } catch (error) {}
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) conversation.messages.push(newMessage._id);
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
