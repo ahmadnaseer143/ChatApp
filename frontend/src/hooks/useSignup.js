9770e3e64e0b22411a4701c6036587698552a562
ahmadnaseer143 <anaseer942@gmail.com>
2025-06-16 15:22:56 -0500

    context added with local storage'

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -36,7 +38,10 @@ const useSignup = () => {
 
       const data = await res.json();
 
-      console.log(data);
+      if (data.error) throw new Error(data.error);
+
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
     } catch (error) {
       toast.error(error.message);
     } finally {
